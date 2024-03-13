590dfe555a805647785bc6781e9b1b3f2133e2a1
pastrelt <pavel.streltsov13@gmail.com>
2024-03-13 17:26:50 +0700

    добавлен класс работы с бд

--- gold/Mountains.py
+++ gold/Mountains.py
@@ -1,4 +1,5 @@
 import psycopg2
+import json
 
 # Establishing the connection
 # Устанавливаем соединение с базой данных PostgreSQL,
@@ -50,22 +51,24 @@ if not table_exists:
             connect TEXT,
             add_time TIMESTAMP,
     
-            user_email VARCHAR(100),
-            user_phone VARCHAR(20),
-            user_fam VARCHAR(100),
-            user_name VARCHAR(100),
-            user_otc VARCHAR(100),
+            email VARCHAR(100),
+            phone VARCHAR(20),
+            fam VARCHAR(100),
+            name VARCHAR(100),
+            otc VARCHAR(100),
     
             latitude VARCHAR(20),
             longitude VARCHAR(20),
             height VARCHAR(20),
     
-            level_winter VARCHAR(10),
-            level_summer VARCHAR(10),
-            level_autumn VARCHAR(10),
-            level_spring VARCHAR(10),
+            winter VARCHAR(10),
+            summer VARCHAR(10),
+            autumn VARCHAR(10),
+            spring VARCHAR(10),
     
-            images JSON
+            images JSON,
+            
+            status VARCHAR(10)
         );
     ''')
     # Фиксируем транзакцию, чтобы изменения были сохранены в базе данных.
@@ -73,3 +76,64 @@ if not table_exists:
 
 # Closing the connection
 conn.close()
+
+# Класс для работы с базой данных
+class Database:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            database="mountains",
+            user='postgres',
+            password='',
+            host='127.0.0.1',
+            port='5432'
+        )
+        self.cur = self.conn.cursor()
+
+    def insert_pereval(self, pereval_data):
+        # Метод insert_pereval принимает словарь pereval_data с информацией о перевале
+        # и вставляет его в базу данных.
+        # Статус модерации устанавливается в "new" при добавлении новой записи.
+        query = '''
+            INSERT INTO my_mountain (beauty_title, title, other_titles, connect, add_time,
+                                     user_email, user_fam, user_name, user_otc, user_phone,
+                                     latitude, longitude, height, level_winter, level_summer,
+                                     level_autumn, level_spring, images, status)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            RETURNING id;
+        '''
+        self.cur.execute(query, (pereval_data['beauty_title'], pereval_data['title'], pereval_data['other_titles'],
+                                 pereval_data['connect'], pereval_data['add_time'], pereval_data['user']['email'],
+                                 pereval_data['user']['fam'], pereval_data['user']['name'], pereval_data['user']['otc'],
+                                 pereval_data['user']['phone'], pereval_data['coords']['latitude'],
+                                 pereval_data['coords']['longitude'], pereval_data['coords']['height'],
+                                 pereval_data['level']['winter'], pereval_data['level']['summer'],
+                                 pereval_data['level']['autumn'], pereval_data['level']['spring'],
+                                 psycopg2.extras.Json(pereval_data['images'], 'new' # начальный статус модерации
+                                 )))
+        inserted_id = self.cur.fetchone()[0]
+        self.conn.commit()
+        return inserted_id
+
+    def update_status(self, pereval_id, new_status):
+        # Mетод update_status принимает идентификатор перевала pereval_id и
+        # новый статус new_status. Перед выполнением обновления проверяется,
+        # что новый статус является одним из допустимых значений.
+        # Затем выполняется SQL-запрос для обновления статуса записи в базе данных.
+        valid_statuses = ['new', 'pending', 'accepted', 'rejected']
+        if new_status not in valid_statuses:
+            return {"status": 400, "message": "Недопустимое значение для статуса", "id": None}
+
+        sql = """
+        UPDATE pereval
+        SET status = %s
+        WHERE id = %s;
+        """
+        values = (new_status, pereval_id)
+
+        try:
+            self.cur.execute(sql, values)
+            self.conn.commit()
+            return {"status": 200, "message": "Статус успешно обновлен", "id": pereval_id}
+        except psycopg2.Error as e:
+            return {"status": 500, "message": str(e), "id": None}
+
