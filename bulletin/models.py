e11d6204dcf860a8e0e3074f2efaaf2060dc2436
pastrelt <pavel.streltsov13@gmail.com>
2024-04-03 20:26:27 +0700

    строю модели +Bulletin +BulletinCategory

--- bulletin/models.py
+++ bulletin/models.py
@@ -35,46 +35,35 @@ class News(models.Model):
 
 # 3. Модель Bulletin(Post)
 # Эта модель должна содержать объявления, которые создают пользователи.
-# Каждое объявление имеет одну категорию.
 # Соответственно, модель должна включать следующие поля:
-# связь «один ко многим» с моделью Author;
-# поле с выбором — «статья» или «новость»;
 # автоматически добавляемая дата и время создания;
 # связь «многие ко многим» с моделью Category (с дополнительной моделью PostCategory);
-# заголовок статьи/новости;
-# текст статьи/новости;
-# рейтинг статьи/новости.
-# class Post(models.Model):
-#     author = models.ForeignKey('Author', on_delete = models.CASCADE)
-#     article_or_news = models.CharField(max_length=7, choices=[('статья', 'статья'), ('новость', 'новость')])
-#     date_and_time = models.DateTimeField(auto_now_add = True)
-#     category = models.ManyToManyField('Category', through = 'PostCategory')
-#     title =  models.CharField(max_length = 255)
-#     text_article_or_news = models.TextField()
-#     rating = models.IntegerField(default = 0)
-#
-#     #  Превью статьи. Возвращает начало статьи (предварительный просмотр) длиной 124 символа и добавляет многоточие в конце.
-#     def preview(self):
-#         preview = self.text_article_or_news[:124] + '...'
-#         return preview
-#
-#     # Форматирование вывода данных для вывода на основную страницу.
-#     def __str__(self):
-#         return (f'{self.title.title()}   '
-#                 f'{self.date_and_time.strftime('%d-%m-%Y %H:%M:%S')}   '
-#                 f'{self.text_article_or_news[:20]}')
-#
-#     def get_absolute_url(self):
-#         return reverse('article_detail', args=[str(self.id)])
-#
-#
-# # 4. Модель PostCategory
-# # Промежуточная модель для связи «многие ко многим»:
-# # связь «один ко многим» с моделью Post;
-# # связь «один ко многим» с моделью Category.
-# class PostCategory(models.Model):
-#     post = models.ForeignKey('Post', on_delete = models.CASCADE)
-#     category = models.ForeignKey('Category', on_delete = models.CASCADE)
+# заголовок объявления;
+# текст объявления.
+class Bulletin(models.Model):
+    author_bulletin = models.ForeignKey(User, on_delete = models.CASCADE)
+    title_bulletin =  models.CharField(max_length = 255)
+    text_bulletin = models.TextField()
+    date_and_time_bulletin = models.DateTimeField(auto_now_add = True)
+    category_bulletin = models.ManyToManyField('Category', through = 'BulletinCategory')
+
+    # Форматирование вывода данных для вывода на основную страницу.
+    def __str__(self):
+        return (f'{self.title.title()}   '
+                f'{self.date_and_time.strftime('%d-%m-%Y %H:%M:%S')}   '
+                f'{self.text_article_or_news[:20]}')
+
+    def get_absolute_url(self):
+        return reverse('article_detail', args=[str(self.id)])
+
+
+# 3.1 Модель BulletinCategory
+# Промежуточная модель для связи «многие ко многим»:
+# связь «один ко многим» с моделью Bulletin;
+# связь «один ко многим» с моделью Category.
+class BulletinCategory(models.Model):
+    post = models.ForeignKey('Bulletin', on_delete = models.CASCADE)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE)
 #
 #
 # # 1. Модель Comment
